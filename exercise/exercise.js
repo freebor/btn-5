95c3f7d2f7f276bde987710e5f4260e5f004d492
Freedom Chisom <chisomnwanoruo6@gmail.com>
2023-09-23 01:31:48 -0700

    new commit

--- exercise/exercise.js
+++ exercise/exercise.js
@@ -14,12 +14,11 @@ function maskify(cc) {
   function isPangram(string) {
     // Remove non-alphabet characters and convert to lowercase
     const cleanSentence = string.toLowerCase();
-    const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","z"];
-    // let alphabet = "abcdefghijklmnopqrstuvwxyz";
-    // let splitAlphabet = alphabet.split("");
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let splitAlphabet = alphabet.split("");
     const uniqueLetters = cleanSentence.split("");
 
-    if (alphabet.every((char) => uniqueLetters.includes(char))) {
+    if (splitAlphabet.every((char) => uniqueLetters.includes(char))) {
       return true
     }else{
       return false
@@ -27,6 +26,84 @@ function maskify(cc) {
     
   }
 
-  console.log(isPangram("The quick brown fox jumps over the lazy dog"));
-  console.log(isPangram("This is not a pangram."));
+  // console.log(isPangram("The quick brown fox jumps over the lazy dog"));
+  // console.log(isPangram("This is not a pangram."));
   
+
+  function formatPhoneNumber(numbers) {
+    const formattedNumber = `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
+
+    if (numbers.length !== 10) {
+      return "numbers should not be above 10 digits";
+    }
+    return formattedNumber;
+  }
+  
+  // Example usage:
+// console.log(formatPhoneNumber([2, 3, 4, 4, 5, 6, 7, 8, 9, 0])); // Output: (123) 456-7890
+
+
+function breakCamelCase(inputString) {
+  let result = "";
+  
+  for (let i = 0; i < inputString.length; i++){
+    
+    const currentChar = inputString[i];
+    // Check if the current character is lowercase and the next character is uppercase
+    if (currentChar == currentChar.toUpperCase()){
+      result+= " ";
+    }
+    result += currentChar   
+  };                      
+  
+  return result;
+}
+
+// Example usage:
+// console.log(breakCamelCase("thisIsCamelCase")); // Output: "this Is Camel Case"
+
+
+function formatTime(seconds) {
+  if (seconds < 0) {
+    return "Invalid input"; // Handle negative input if needed
+  }
+
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  const formattedHours = hours.toString().padStart(2, '0');
+  const formattedMinutes = minutes.toString().padStart(2, '0');
+  const formattedSeconds = remainingSeconds.toString().padStart(2, '0');
+
+  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
+
+
+// Example usage:
+// const seconds =  4; // Replace with your desired number of seconds
+// const formattedTime = formatTime(seconds);
+// console.log(formattedTime); // Output: "01:01:01"
+
+
+function findPeaks(arr) {
+  const peaks = [];
+  const positions = [];
+
+  for (let i = 1; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+      peaks.push(arr[i]);
+      positions.push(i);
+    }
+  }
+
+  return { pos: positions, peaks };
+}
+
+// Example usage:
+const numericArray = [1,2,5,4,3,2,3,6,4,1,2,3,3,4,5,3,2,1,2,3,5,5,4,3];
+const result = findPeaks(numericArray);
+console.log(result);
+
+
+
--- week1 task/app.js
+++ week1 task/app.js
@@ -1,61 +1,91 @@
-let items = document.querySelectorAll('.content');
-let grid = document.querySelectorAll('.grid-content');
-
-let section = document.querySelectorAll('section');
-
-
-
-items.forEach(item=>{
-    item.addEventListener("mousemove", (e)=>{
-        // get position pointer in with (pixel)
-        let positionPx = e.x - item.getBoundingClientRect().left;
-        let positionX = (positionPx / item.offsetWidth)*100;
-        // get position pointer in height (px)
-        let positionPy = e.y - item.getBoundingClientRect().top;
-        // convert to %
-        let positionY = (positionPy / item.offsetHeight)*100;
-
-        item.style.setProperty('--rX', (0.5)*(50 - positionY) + "deg")
-        item.style.setProperty('--rY', -(0.5)*(50 - positionX) + "deg")
-        item.style.setProperty("--sU", (1.1));
-    })
-    item.addEventListener('mouseout', ()=>{
-        item.style.setProperty("--sU", (1.0));
-        item.style.setProperty('--rX', "0deg")
-        item.style.setProperty('--rY', "0deg")
-    })
-})
-grid.forEach(item=>{
-    item.addEventListener("mousemove", (e)=>{
-        // get position pointer in with (pixel)
-        let positionPx = e.x - item.getBoundingClientRect().left;
-        let positionX = (positionPx / item.offsetWidth)*100;
-        // get position pointer in height (px)
-        let positionPy = e.y - item.getBoundingClientRect().top;
-        // convert to %
-        let positionY = (positionPy / item.offsetHeight)*100;
-
-        item.style.setProperty('--rX', -(0.3)*(30 - positionY) + "deg")
-        item.style.setProperty('--rY', (0.3)*(30 - positionX) + "deg")
-    })
-    item.addEventListener('mouseout', ()=>{
-        item.style.setProperty('--rX', "0deg")
-        item.style.setProperty('--rY', "0deg")
-    })
-})
+const blogSection = document.querySelectorAll(".blog");
+const blogFooter = document.querySelectorAll(".blogFooter");
+const likeBtns = document.querySelectorAll(".like-btn");
+const likeCounter = document.querySelector(".likeCounter");
+const blogTextHeader = document.getElementById('text-header');
+const blogTextCutBody = document.getElementById('text-cut-body');
+const blog = document.getElementById('blog-section');
 
 
-window.onscroll = () => {
-    section.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
+let content = [
+    {
+        id : 1,
+        Header: "What is TRUE TONE on the iPhone?",
+        image : "img/iPhone-14-Pro-cameras-390x220.jpeg",
+        like : 0,
+        disLike : 0,
+        link : "siri.html",
+        shortContent: "Have you ever wondered how your iPhone's flash can capture photos with more natural and uniform lighting? The answer lies in the True Tone flash, an innovative technology present in the latest models of the iPhone.",
+        fullContent: "",
+    },
+    {
+        id : 2,
+        Header: "Keeping Your Photos and Videos Private on Your iPhone or iPad",
+        image : "img/Hide-photos-iOS-390x220.jpg",
+        like : 0,
+        disLike : 0,
+        link : "siri.html",
+        shortContent: "The iPhone has become the most personal and intimate device we have nowadays, as it carries practically all our digital life within it.",
+        fullContent: "",
+    },
+    {
+        id : 3,
+        Header: "Siri Offline Requests Expanding to More Languages on iOS 17",
+        image : "img/siri.jpg",
+        like : 0,
+        disLike : 0,
+        link : "siri.html",
+        shortContent: "Apple is set to introduce an exciting update to its virtual assistant, Siri, with the upcoming release of iOS 17. The highly anticipated feature will expand Siri’s offline capabilities to include a wider range of languages...",
+        fullContent: "",
+    },
+    
+]
 
-        if (top >= offset && top < offset + height) {
-            sec.classList.add('show-animate');
-        }
-        // else{
-        //     sec.classList.remove('show-animate');
-        // }
-    })
-};
+
+// like section counter 
+let likeCount = (id,element) =>{
+    let filteredContent = content.find((c)=> c.id == id)
+    if (filteredContent) {
+        filteredContent.like = filteredContent.like + 1;
+        element.innerHTML = filteredContent.like;
+    }
+}
+let dislikeCount = (id,element) =>{
+    let filteredContent = content.find((c)=> c.id == id)
+    if (filteredContent) {
+        filteredContent.disLike = filteredContent.disLike + 1;
+        element.innerHTML = filteredContent.disLike;
+    }
+}
+for (let i = 0; i <= content.length; i++) {
+    let eachContent = content[i];
+    let uniqueLike = "like" + eachContent.id;
+    let uniqueDislike = "disLike" + eachContent.id;
+    blog.innerHTML += `<div class="blog">
+    <div id="text-header">${eachContent.Header}</div>
+    <div class="blog-img-wrap">
+        <img src="${eachContent.image}" alt="img" class="img-response">
+    </div>
+    <p class="blog-text" id="text-cut-body">${eachContent.shortContent}</p>
+    <a href="${eachContent.link}" class="continue">CONTINUE READING</a>
+    <div class="blogFooter">
+        <div class="likes">
+            likes:
+            <div class="like-btn" onclick="likeCount(${eachContent.id},${uniqueLike})">
+                <i class="fa-regular fa-thumbs-up"></i>
+                <span id="${uniqueLike}">${eachContent.like}</span>
+            </div>
+            <div class="like-btn" onclick="dislikeCount(${eachContent.id},${uniqueDislike})">
+                <i class="fa-regular fa-thumbs-down"></i>
+                <span id="${uniqueDislike}">${eachContent.disLike}</span>
+            </div>
+        </div>
+        <p class="blog-comment">14 comment</p>
+    </div>
+    </div>`
+}
+
+
+
+// console.log(blogTextHeader);
+// blogTextHeader.innerHTML = jsonHeader(content);
